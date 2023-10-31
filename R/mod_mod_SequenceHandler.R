ffbba4b95886408918669bd953093d80c68cc8ee
chuanghembo <s233427@dtu.dk>
2023-10-31 09:58:19 +0100

    Mod commit

---
+++ R/mod_mod_SequenceHandler.R
@@ -0,0 +1,31 @@
+#' mod_SequenceHandler UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd 
+#'
+#' @importFrom shiny NS tagList 
+mod_mod_SequenceHandler_ui <- function(id){
+  ns <- NS(id)
+  tagList(
+ 
+  )
+}
+    
+#' mod_SequenceHandler Server Functions
+#'
+#' @noRd 
+mod_mod_SequenceHandler_server <- function(id){
+  moduleServer( id, function(input, output, session){
+    ns <- session$ns
+ 
+  })
+}
+    
+## To be copied in the UI
+# mod_mod_SequenceHandler_ui("mod_SequenceHandler_1")
+    
+## To be copied in the server
+# mod_mod_SequenceHandler_server("mod_SequenceHandler_1")
